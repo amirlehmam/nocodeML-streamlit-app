33c058ed4b2698812f13b334c8c045a6dd1a2e7f
No Brainer <aeonthings@gmail.com>
2024-06-07 17:37:09 +0200

    z

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ authenticator = stauth.Authenticate(
     config['pre-authorized']
 )
 
-authenticator.login()
+authenticator.login('Login', 'main')
 
 if st.session_state["authentication_status"]:
     authenticator.logout()
