316bd9942e6421a69ff155a635ef2b43642c57ea
No Brainer <aeonthings@gmail.com>
2024-06-07 16:08:46 +0200

    z

--- setup_db.py
+++ setup_db.py
@@ -7,11 +7,15 @@ with open('config.yaml', 'r') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 # Hash the password
-hashed_password = generate_password_hash('admin123', method='pbkdf2:sha256')
+hashed_password_admin = generate_password_hash('Azerty_94400', method='pbkdf2:sha256')
+hashed_password_test_user = generate_password_hash('Azerty_94', method='pbkdf2:sha256')
 
-# Update the config with hashed password
-config['credentials']['usernames']['admin']['password'] = hashed_password
+# Update the config with hashed passwords
+config['credentials']['usernames']['admin']['password'] = hashed_password_admin
+config['credentials']['usernames']['test_user']['password'] = hashed_password_test_user
 
 # Save the updated config
 with open('config.yaml', 'w') as file:
     yaml.dump(config, file)
+
+print("Passwords hashed and config.yaml updated successfully.")
