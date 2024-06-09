38985fea2d898a6defdc7c2fd6f8f1508bbc6809
amirlehmam <aeonthings@gmail.com>
2024-06-09 17:12:59 +0000

    Updated the app with recent changes

---
+++ webhook_listener.py
@@ -0,0 +1,43 @@
+from flask import Flask, request
+
+import os
+
+import logging
+
+import threading
+
+
+logging.basicConfig(filename='/home/ec2-user/nocodeML-streamlit-app/webhook_listener.log', level=logging.INFO)
+
+
+app = Flask(__name__)
+
+
+def run_update_script():
+
+    os.system('/home/ec2-user/nocodeML-streamlit-app/update_and_restart.sh')
+
+
+@app.route('/payload', methods=['POST'])
+
+def payload():
+
+    if request.method == 'POST':
+
+        logging.info('Received webhook payload')
+
+        threading.Thread(target=run_update_script).start()
+
+        return 'Success', 200
+
+    else:
+
+        logging.info('Invalid request')
+
+        return 'Invalid Request', 400
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5000)
+
