d7bb66e0d64ab7a9fa3b16f82d83750fe31f85fd
No Brainer <aeonthings@gmail.com>
2024-06-11 01:10:28 +0200

    z

--- scripts/advanced_model_exploration.py
+++ scripts/advanced_model_exploration.py
@@ -199,7 +199,8 @@ def run_advanced_model_exploration():
             st.subheader("Neural Network Parameters")
             epochs = st.slider("Number of Epochs", min_value=10, max_value=1000, value=100)
             batch_size = st.slider("Batch Size", min_value=10, max_value=128, value=32)
-            model = KerasClassifier(model=create_nn_model, input_dim=st.session_state.X_train.shape[1], epochs=epochs, batch_size=batch_size, verbose=0)
+            input_dim = st.session_state.X_train.shape[1]
+            model = KerasClassifier(model=create_nn_model, input_dim=input_dim, epochs=epochs, batch_size=batch_size, verbose=0)
         
         elif model_type == "Stacking Ensemble":
             st.subheader("Stacking Ensemble Parameters")
