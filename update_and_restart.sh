1bab68054354a2c512b1322f01eb258fffc35a8a
EC2 Default User <ec2-user@ip-172-31-79-139.ec2.internal>
2024-06-18 16:15:32 +0000

    Your commit message

--- update_and_restart.sh
+++ update_and_restart.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 
-
-LOGFILE=~/nocodeML-streamlit-app/update_and_restart.log
-
+LOGFILE=~/NoCodeML/update_and_restart.log
 
 echo "Starting update and restart script" >> $LOGFILE
 
-
 # Navigate to the project directory
 
-cd ~/nocodeML-streamlit-app
-
+cd ~/NoCodeML
 
 echo "Pulling latest changes from git repository" >> $LOGFILE
 
 git pull origin main >> $LOGFILE 2>&1
 
-
 echo "Rebuilding Docker containers" >> $LOGFILE
 
-docker-compose up --build -d >> $LOGFILE 2>&1
-
+docker-compose build -d --no-cache >> $LOGFILE 2>&1
 
 echo "Restarting Docker containers" >> $LOGFILE
 
 docker-compose down >> $LOGFILE 2>&1
 
 docker-compose up -d >> $LOGFILE 2>&1
-
 
 echo "Update and restart complete" >> $LOGFILE
 
